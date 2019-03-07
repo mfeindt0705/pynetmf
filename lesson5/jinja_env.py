722c098e3730e3a5c224739bfaacbccf2390a401
mfeindt0705 <marcus_feindt@outlook.de>
2019-03-07 10:52:46 -0800

    add some jinja files

---
+++ lesson5/jinja_env.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+simple exercise for jinja rendering
+"""
+
+from __future__ import unicode_literals, print_function
+from jinja2 import FileSystemLoader, StrictUndefined
+from jinja2.environment import Environment
+
+# this configuration would use the local directory only
+env = Environment()
+env.loader = FileSystemLoader('.')
+
+# this is adding more directories
+#env = Environment(undefined=StrictUndefined)
+#env.loader = FileSystemLoader([".", "./templates/"])
+
+my_vars = {"bgp_as": 22, 
+           "router_id": "1.1.1.1", 
+           "peer1": "10.20.30.1"}
+
+template_file = "bgp_config.j2"
+template = env.get_template(template_file)
+output = template.render(**my_vars)
+print(output)
+
---
+++ lesson5/jinja_render_1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+exercise for simple rendering
+"""
+from jinja2 import Template
+
+text1 = """
+this is some template
+that has multiple lines
+of text in it
+
+"""
+
+bgp_config = """
+router bgp 42
+ bgp router-id 10.220.88.20
+ bgp log-neighbor-changes
+ neighbor 10.220.88.38 remote-as 44
+"""
+
+j2_template = Template(bgp_config)
+output = j2_template.render()
+print(output)
+
+j2_template = Template(text1)
+output = j2_template.render()
+print(output)
+
+
